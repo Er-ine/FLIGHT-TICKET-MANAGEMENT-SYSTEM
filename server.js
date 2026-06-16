9b790f2eed150df8dc731623853816b4b0bf8562
Er-ine <erineabinu@gmail.com>
2026-06-16 23:45:43 +0530

    Prepare for deployment

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
+app.use(express.static('public'));
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname)));
