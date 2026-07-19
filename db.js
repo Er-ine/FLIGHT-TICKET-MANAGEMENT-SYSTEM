4c01d47f0ef5ba81a635fc0741db4e32fb7397d9
Erine Anna Binu <erineabinu@gmail.com>
2026-07-19 07:45:36 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,11 @@
 const mysql = require("mysql2/promise");
 
+console.log('Connecting to DB with config:');
+console.log('HOST:', process.env.DB_HOST);
+console.log('PORT:', process.env.DB_PORT);
+console.log('USER:', process.env.DB_USER);
+console.log('DATABASE:', process.env.DB_NAME);
+
 const db = mysql.createPool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
